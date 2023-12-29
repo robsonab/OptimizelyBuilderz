aa98632258e0dcaec5e9219423df20f939aa4a36
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2023-12-29 12:18:16 -0300

    Navigation Menu

---
+++ Business/ExtensionMethods/ContentExtensions.cs
@@ -0,0 +1,56 @@
+﻿using EPiServer.Filters;
+using EPiServer.Framework.Web;
+using EPiServer.ServiceLocation;
+
+namespace Builderz.Business.ExtensionMethods
+{
+    /// <summary>
+    /// Extension methods for content
+    /// </summary>
+    public static class ContentExtensions
+    {
+        /// <summary>
+        /// Shorthand for IContentLoader.Get
+        /// </summary>
+        /// <typeparam name="TContent"></typeparam>
+        /// <param name="contentLink"></param>
+        /// <returns></returns>
+        public static IContent Get<TContent>(this ContentReference contentLink) where TContent : IContent
+        {
+            var loader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            return loader.Get<TContent>(contentLink);
+        }
+
+        /// <summary>
+        /// Filters content which should not be visible to the user. 
+        /// </summary>
+        public static IEnumerable<T> FilterForDisplay<T>(this IEnumerable<T> contents, bool requirePageTemplate = false, bool requireVisibleInMenu = false)
+            where T : IContent
+        {
+            var accessFilter = new FilterAccess();
+            var publishedFilter = new FilterPublished();
+            contents = contents.Where(x => !publishedFilter.ShouldFilter(x) && !accessFilter.ShouldFilter(x));
+            if (requirePageTemplate)
+            {
+                var templateFilter = ServiceLocator.Current.GetInstance<FilterTemplate>();
+                templateFilter.TemplateTypeCategories = TemplateTypeCategories.Request;
+                contents = contents.Where(x => !templateFilter.ShouldFilter(x));
+            }
+            if (requireVisibleInMenu)
+            {
+                contents = contents.Where(x => VisibleInMenu(x));
+            }
+            return contents;
+        }
+
+        private static bool VisibleInMenu(IContent content)
+        {
+            if (content is not PageData page)
+            {
+                return true;
+            }
+
+            return page.VisibleInMenu;
+        }
+    }
+}
