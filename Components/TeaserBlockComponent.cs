a1c32f692668bb39ed2da635cad336ab04b2a47e
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2023-12-29 17:05:26 -0300

    TeaserBlock

---
+++ Components/TeaserBlockComponent.cs
@@ -0,0 +1,23 @@
+﻿using Builderz.Models.Blocks;
+using Builderz.Models.ViewModels;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Builderz.Components
+{
+    [TemplateDescriptor(AvailableWithoutTag = true)]
+    public class TeaserBlockComponent : BlockComponent<TeaserBlock>
+    {
+        protected override IViewComponentResult InvokeComponent(TeaserBlock currentBlock)
+        {
+            TeaserBlockViewModel viewmodel = new()
+            {
+                CurrentBlock = currentBlock,
+                TodaysVisitorCount = new Random().Next(300, 900)
+            };
+            return View(viewmodel);
+        }
+    }
+}
+
--- Models/Pages/StandardPage.cs
+++ Models/Pages/StandardPage.cs
@@ -7,6 +7,9 @@ namespace Builderz.Models.Pages
          Description = "Use this page type unless you need a more specialized one.")]    
     public class StandardPage : SitePageData
     {
-               
+        [Display(Name = "Main content area",
+           Description = "The main content area contains an ordered collection to content references, for example blocks, media assets, and pages.",
+           GroupName = SystemTabNames.Content, Order = 30)]        
+        public virtual ContentArea MainContentArea { get; set; }
     }
 }
