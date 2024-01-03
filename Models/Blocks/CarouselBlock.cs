2e60db06ec7a128c5523f325476f389c6a6ed304
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2024-01-03 16:51:49 -0300

    Carousel / Slides

---
+++ Models/Blocks/CarouselBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Builderz.Models.Blocks
+{
+    [ContentType(DisplayName = "Carousel", GroupName = SiteGroupNames.Common)]
+    public class CarouselBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Previous Text", Order = 10, GroupName = SystemTabNames.PageHeader)]
+        public virtual string PreviousText { get; set; }
+        
+        [CultureSpecific]
+        [Display(Name = "Next Text", Order = 20, GroupName = SystemTabNames.PageHeader)]
+        public virtual string NextText { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Slides", Order = 30)]
+        [AllowedTypes(typeof(SlideBlock))]
+        public virtual ContentArea  Slides{ get; set; }
+
+    }
+}
